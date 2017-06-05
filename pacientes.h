ea1209a24b074103359f75be4aff610a5c6b9a0d
Adjucaris <importantinho_2@hotmail.com>
2017-06-05 18:35:00 -0300

    Update pacientes.h

--- pacientes.h
+++ pacientes.h
@@ -4,8 +4,10 @@
 #include "lista_enc.h"
 #include "fila.h"
 
-typedef struct dados pessoas_lista;
+typedef struct pacientes paciente_t;
 
 lista_enc_t *ler_arquivo(char *nome_arq);
+
+paciente_t *cria_paciente(char *nome, int idade, char sexo, char *diagnostico, int prioridade, int chance);
 
 #endif /* PACIENTES_H_ */
