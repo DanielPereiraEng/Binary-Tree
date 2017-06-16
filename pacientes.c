d8eb0cdbf9b788258b457a89e6fa35977ecf7cd7
Adjucaris <importantinho_2@hotmail.com>
2017-06-15 23:57:49 -0300

    Update pacientes.c

--- pacientes.c
+++ pacientes.c
@@ -74,6 +74,7 @@ lista_enc_t *ler_arquivo(char *nome){
 	return lista;
 }
 
+
 paciente_t * cria_paciente(char *nome, int idade, char sexo, char *diagnostico, int prioridade, int chance){
 
 	paciente_t *p;
@@ -105,8 +106,94 @@ paciente_t * cria_paciente(char *nome, int idade, char sexo, char *diagnostico, 
 	strncpy(p->nome, nome, strlen(nome) + 1);
 	p->idade = idade;
 	p->sexo = sexo;
+	strncpy(p->diagnostico, diagnostico, strlen(diagnostico) + 1);
 	p->prioridade = prioridade;
 	p->chance = chance;
 
 	return p;
+}
+
+
+fila_t* cria_fila_pacientes (lista_enc_t *lista){
+    no_t *meu_no;
+    paciente_t *paciente_atual;
+    fila_t *fila;
+    int x, tamanho;
+
+    fila = cria_fila ();
+
+    tamanho = obtem_tamanho(lista);
+
+    printf("NUMERO DE PESSOAS NESSE NEGOCIO: %d\n", tamanho);
+
+    meu_no = obtem_cabeca(lista);
+
+    for(x = 0; x <= tamanho-1; x++){
+        paciente_atual = obter_dado(meu_no);
+        enqueue(paciente_atual,fila);
+        meu_no = obter_proximo(meu_no);
+    }
+
+    return fila;
+}
+
+
+int obter_idade(paciente_t *p){
+
+	if (p == NULL){
+		fprintf(stderr, "get_idade: p inv'alido!\n");
+		exit(EXIT_FAILURE);
+	}
+
+	return p->idade;
+}
+
+char obter_sexo(paciente_t *p){
+
+	if (p == NULL){
+		fprintf(stderr, "get_sexo: p inv'alido!\n");
+		exit(EXIT_FAILURE);
+	}
+
+	return p->sexo;
+}
+
+char * obter_nome(paciente_t *p){
+
+	if (p == NULL){
+		fprintf(stderr, "get_nome: p inv'alido!\n");
+		exit(EXIT_FAILURE);
+	}
+
+	return p->nome;
+}
+
+int obter_prioridade(paciente_t *p){
+
+	if (p == NULL){
+		fprintf(stderr, "get_idade: p inv'alido!\n");
+		exit(EXIT_FAILURE);
+	}
+
+	return p->prioridade;
+}
+
+int obter_chance(paciente_t *p){
+
+	if (p == NULL){
+		fprintf(stderr, "get_sexo: p inv'alido!\n");
+		exit(EXIT_FAILURE);
+	}
+
+	return p->chance;
+}
+
+char * obter_diagnostico(paciente_t *p){
+
+	if (p == NULL){
+		fprintf(stderr, "get_nome: p inv'alido!\n");
+		exit(EXIT_FAILURE);
+	}
+
+	return p->diagnostico;
 }
