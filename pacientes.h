4d6959645d023c1f9fcad4a3048a7d110abde9c9
Adjucaris <importantinho_2@hotmail.com>
2017-06-16 02:25:11 -0300

    Update pacientes.h

--- pacientes.h
+++ pacientes.h
@@ -10,6 +10,8 @@ lista_enc_t *ler_arquivo(char *nome_arq);
 
 paciente_t *cria_paciente(char *nome, int idade, char sexo, char *diagnostico, int prioridade, int chance);
 
+fila_t* cria_fila_pacientes (lista_enc_t *lista);
+
 char * obter_nome(paciente_t *p);
 char obter_sexo(paciente_t *p);
 int obter_idade(paciente_t *p);
@@ -17,6 +19,6 @@ char * obter_diagnostico(paciente_t *p);
 int obter_prioridade(paciente_t *p);
 int obter_chance(paciente_t *p);
 
-fila_t* cria_fila_pacientes (lista_enc_t *lista);
+void libera_dados_list_enc(lista_enc_t *lista);
 
 #endif /* PACIENTES_H_ */
