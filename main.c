f63d64edb5becee61b261c3e47e232277ed61070
Adjucaris <importantinho_2@hotmail.com>
2017-06-15 23:59:27 -0300

    Update main.c

--- main.c
+++ main.c
@@ -7,9 +7,18 @@ int main()
 {
     lista_enc_t *lista;
     fila_t *fila;
-    no_t *no;
+    paciente_t *paciente_atual;
+    int x, tamanho;
 
     lista = ler_arquivo("Pacientes.csv");
+
+    fila = cria_fila_pacientes(lista);
+
+    while((fila)!=NULL){
+        paciente_atual = dequeue(fila);
+        printf("%s - %d \n", obter_nome(paciente_atual), obter_idade(paciente_atual));}
+
+    printf("Ponto de Teste");
 
     return 0;
 }
