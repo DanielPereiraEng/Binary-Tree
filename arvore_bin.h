04bfe10a87f76a4d2c32a3a5db908a183f15b470
Adjucaris <importantinho_2@hotmail.com>
2017-06-17 03:36:51 -0300

    Update arvore_bin.h

--- arvore_bin.h
+++ arvore_bin.h
@@ -1,6 +1,10 @@
 #ifndef ARVORE_BIN_H_
 #define ARVORE_BIN_H_
 
+#include "pacientes.h"
+
 typedef struct arvore arvore_t;
+
+void inserir(arvore_t **tree ,paciente_t *paciente_atual);
 
 #endif /* ARVORE_BIN_H_ */
