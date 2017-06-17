f44f614f54887fa12663d943dfe54dfa90abff6f
Adjucaris <importantinho_2@hotmail.com>
2017-06-17 04:41:51 -0300

    Update arvore_bin.c

--- arvore_bin.c
+++ arvore_bin.c
@@ -9,46 +9,46 @@ struct arvore {
     void* dados;
     struct arvore * right, * left;
 };
-typedef struct arvore arvore_t;
 
-void inserir(arvore_t **tree ,paciente_t *paciente_atual)
-{
+
+void inserir(arvore_t **tree ,paciente_t *paciente_atual){
+
+    printf("IIIII\n");
+
     arvore_t *temp = NULL;
-    if(!(*tree))
-    {
+    if(!(*tree)){
         temp = (arvore_t *)malloc(sizeof(arvore_t));
         temp->left =  NULL;
         temp->right = NULL;
         temp->dados = paciente_atual;
         *tree = temp;
-        return;
-    }
+        return;}
 
-    paciente_t* pessoal_alvo;
+    printf("OOOOO\n");
 
-    pessoal_alvo = temp->dados;
+//    paciente_t* pessoal_alvo;
 
-    if(obter_prioridade(paciente_atual) < obter_prioridade(pessoal_alvo))
-    {
-        inserir(&(*tree)->left, paciente_atual);
-    }
-    else if(paciente_atual > (*tree)->dados)
-    {
-        inserir(&(*tree)->right, paciente_atual);
-    }
+//    pessoal_alvo = temp->dados;
 
+//    if(obter_prioridade(paciente_atual) < obter_prioridade(pessoal_alvo)){
+//        inserir(&(*tree)->left, paciente_atual);}
+
+//    else if(obter_prioridade(paciente_atual)  > obter_prioridade(pessoal_alvo)){
+//        inserir(&(*tree)->right, paciente_atual);}
 }
 
-//void print_preorder(arvore_t * tree)
-//{
-//    if (tree)
-//    {
-//        printf("%d\n",tree->dados);
-//        print_preorder(tree->left);
-//        print_preorder(tree->right);
-//    }
+void print_preorder(arvore_t *tree){
 
-//}
+    paciente_t *paciente;
+
+    paciente = tree->dados;
+
+    if (tree){
+        printf("%s\n",obter_nome(paciente));
+        print_preorder(tree->left);
+        print_preorder(tree->right);}
+
+}
 
 //void print_inorder(arvore_t * tree)
 //{
@@ -79,3 +79,4 @@ void inserir(arvore_t **tree ,paciente_t *paciente_atual)
 //        free(tree);
 //    }
 //}
+
