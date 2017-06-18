07b0d889073ad4e89e0ceb5f13b6f711aa14b264
Adjucaris <importantinho_2@hotmail.com>
2017-06-18 20:05:58 -0300

    Update arvore_bin.h

--- arvore_bin.h
+++ arvore_bin.h
@@ -2,7 +2,6 @@
 #define ARVORE_BIN_H_
 
 #include "pacientes.h"
-#include "pilha.h"
 
 typedef struct arvore arvore_t;
 
@@ -12,5 +11,9 @@ void inserir(arvore_t **tree , paciente_t *paciente_atual);
 void print_preorder(arvore_t *tree);
 void print_inorder(arvore_t *tree);
 void print_posorder(arvore_t *tree);
+
+arvore_t *heapsort_loop(arvore_t *tree, int tamanho);
+
+arvore_t *heapsort(arvore_t *tree);
 
 #endif /* ARVORE_BIN_H_ */
