9af55cd50e8d074b6ccfc91816d4c1374a8b1704
Adjucaris <importantinho_2@hotmail.com>
2017-06-18 03:55:45 -0300

    Update arvore_bin.c

--- arvore_bin.c
+++ arvore_bin.c
@@ -9,102 +9,67 @@
 struct arvore {
     void *dados;
     struct arvore *pai, * right, * left;
-    int indice;
 };
 
+void inserir(arvore_t* *tree , paciente_t *paciente_atual){
 
-void inicializar(arvore_t **tree ,paciente_t *paciente_atual, pilha_t *main_stack){
+    arvore_t* temp = *tree;
 
-    arvore_t *temp = NULL;
-
-    if(!(*tree)){
-        temp = (arvore_t *)malloc(sizeof(arvore_t));
-        temp->left =  NULL;
-        temp->right = NULL;
-        temp->pai = NULL;
-        temp->dados = paciente_atual;
-        temp->indice = 1;
-        push(1, main_stack);
-        *tree = temp;
-        return;}
-
-}
-
-
-void inserir(arvore_t **tree , paciente_t *paciente_atual, pilha_t *main_stack, int indice){
-
-    paciente_t* pessoal_alvo;
-    arvore_t *temp;
-    arvore_t *remix;
-
-    temp = *tree;
-    pessoal_alvo = temp->dados;
-
-    temp = NULL;
-
-    if(!(*tree)){
+    if(temp == NULL){
         temp = (arvore_t *)malloc(sizeof(arvore_t));
         temp->left =  NULL;
         temp->right = NULL;
         temp->dados = paciente_atual;
-        temp->pai = pessoal_alvo;
-        temp->indice = indice;
         *tree = temp;
-        pop(main_stack);
-        return;}
-
-    if(temp->left == NULL){
-        push(indice, main_stack);
-        inserir(&(*tree)->left, paciente_atual, main_stack, indice);}
-
-    if(temp->right == NULL){
-        push(indice, main_stack);
-        inserir(&(*tree)->right, paciente_atual, main_stack, indice);}
-
-//    temp = *tree;
-
-//    pessoal_alvo = temp->dados;
-
-    //printf("%s\n", obter_nome(pessoal_alvo));
-
-//    if(obter_prioridade(paciente_atual) < obter_prioridade(pessoal_alvo)){
-//        inserir(&(*tree)->left, paciente_atual);}
-
-//    else if(obter_prioridade(paciente_atual)  >= obter_prioridade(pessoal_alvo)){
-//        inserir(&(*tree)->right, paciente_atual);}
-}
+    }else{
+        if(paciente_atual < temp->dados){
+            inserir(&temp->left,paciente_atual);
+        }else{
+            inserir(&temp->right,paciente_atual);
+        }
+    }}
 
 void print_preorder(arvore_t *tree){
 
+    if (tree == NULL)
+    {
+        return;
+    }
+
     paciente_t *paciente;
-
     paciente = tree->dados;
-
-    if (tree){
-        printf("%s\n", obter_nome(paciente));
-        print_preorder(tree->left);
-        print_preorder(tree->right);}
+    printf("%s\n", obter_nome(paciente));
+    print_preorder(tree->left);
+    print_preorder(tree->right);
 }
 
-//void print_inorder(arvore_t * tree)
-//{
-//    if (tree)
-//    {
-//        print_inorder(tree->left);
-//        printf("%d\n",tree->dados);
-//        print_inorder(tree->right);
-//    }
-//}
+void print_inorder(arvore_t *tree){
 
-//void print_postorder(arvore_t * tree)
-//{
-//    if (tree)
-//    {
-//        print_postorder(tree->left);
-//        print_postorder(tree->right);
-//        printf("%d\n",tree->dados);
-//    }
-//}
+    if (tree == NULL)
+    {
+        return;
+    }
+
+    paciente_t *paciente;
+    paciente = tree->dados;
+    print_inorder(tree->left);
+    printf("%s\n", obter_nome(paciente));
+    print_inorder(tree->right);
+}
+
+void print_posorder(arvore_t *tree){
+
+    if (tree == NULL)
+    {
+        return;
+    }
+
+    paciente_t *paciente;
+    paciente = tree->dados;
+    print_posorder(tree->left);
+    print_posorder(tree->right);
+    printf("%s\n", obter_nome(paciente));
+}
 
 //void deltree(arvore_t * tree)
 //{
