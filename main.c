be22efd4324b4b6eaf3372a58a7291858cb49166
Adjucaris <importantinho_2@hotmail.com>
2017-06-18 03:54:07 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,46 +1,83 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-#include "pacientes.h"
 #include "arvore_bin.h"
+#include "pacientes.h"
 #include "pilha.h"
 
-int main()
-{
-    lista_enc_t *lista;
-    fila_t *fila;
-    paciente_t *paciente_atual;
-    int x, tamanho, *nivel=0;
-    arvore_t *arvore_binaria;
-    pilha_t *main_stack = cria_pilha();
+struct arvore {
+    void *dados;
+    struct arvore *pai, * right, * left;
+};
 
-    arvore_binaria = NULL;
+void inserir(arvore_t* *tree , paciente_t *paciente_atual){
 
-    lista = ler_arquivo("Pacientes.csv");
+    arvore_t* temp = *tree;
 
-    fila = cria_fila_pacientes(lista);
+    if(temp == NULL){
+        temp = (arvore_t *)malloc(sizeof(arvore_t));
+        temp->left =  NULL;
+        temp->right = NULL;
+        temp->dados = paciente_atual;
+        *tree = temp;
+    }else{
+        if(paciente_atual < temp->dados){
+            inserir(&temp->left,paciente_atual);
+        }else{
+            inserir(&temp->right,paciente_atual);
+        }
+    }}
 
-    tamanho = obtem_tamanho(lista);
+void print_preorder(arvore_t *tree){
 
-    paciente_atual = dequeue(fila);
-
-    inicializar(&arvore_binaria, paciente_atual, main_stack);
-
-    for(x = 2; x <= tamanho; x++){
-        paciente_atual = dequeue(fila);
-        //printf("%s - %d \n", obter_nome(paciente_atual), obter_idade(paciente_atual));
-        inserir(&arvore_binaria, paciente_atual, main_stack, x);
+    if (tree == NULL)
+    {
+        return;
     }
 
-//    print_preorder(arvore_binaria);
-
-    printf("%d\n", nivel);
-
-    libera_dados_list_enc(lista);
-
-    libera_fila(fila);
-
-    printf("Ponto de Teste");
-
-    return 0;
+    paciente_t *paciente;
+    paciente = tree->dados;
+    printf("%s\n", obter_nome(paciente));
+    print_preorder(tree->left);
+    print_preorder(tree->right);
 }
+
+void print_inorder(arvore_t *tree){
+
+    if (tree == NULL)
+    {
+        return;
+    }
+
+    paciente_t *paciente;
+    paciente = tree->dados;
+    print_inorder(tree->left);
+    printf("%s\n", obter_nome(paciente));
+    print_inorder(tree->right);
+}
+
+void print_posorder(arvore_t *tree){
+
+    if (tree == NULL)
+    {
+        return;
+    }
+
+    paciente_t *paciente;
+    paciente = tree->dados;
+    print_posorder(tree->left);
+    print_posorder(tree->right);
+    printf("%s\n", obter_nome(paciente));
+}
+
+//void deltree(arvore_t * tree)
+//{
+//    if (tree)
+//    {
+//        deltree(tree->left);
+//        deltree(tree->right);
+//        free(tree);
+//    }
+//}
+
