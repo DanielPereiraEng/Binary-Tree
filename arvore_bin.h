6f76029bac66f1426094e7a3d192b2b3d3a3c29b
Adjucaris <importantinho_2@hotmail.com>
2017-06-18 01:51:44 -0300

    Update arvore_bin.h

--- arvore_bin.h
+++ arvore_bin.h
@@ -2,10 +2,13 @@
 #define ARVORE_BIN_H_
 
 #include "pacientes.h"
+#include "pilha.h"
 
 typedef struct arvore arvore_t;
 
-void inserir(arvore_t **tree ,paciente_t *paciente_atual);
+void inicializar(arvore_t **tree ,paciente_t *paciente_atual, pilha_t *main_stack);
+
+void inserir(arvore_t **tree , paciente_t *paciente_atual, pilha_t *main_stack, int indice);
 
 void print_preorder(arvore_t *tree);
 
