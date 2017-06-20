68f6496cadbf77f5cc407b2651d98498ff891141
Adjucaris <importantinho_2@hotmail.com>
2017-06-20 05:30:33 -0300

    Update main.c

--- main.c
+++ main.c
@@ -3,45 +3,43 @@
 
 #include "pacientes.h"
 #include "arvore_bin.h"
+#include "lista_enc.h"
+#include "no.h"
 
 int main()
 {
     lista_enc_t *lista;
-    fila_t *fila;
+    no_t *no;
     paciente_t *paciente_atual;
     int x, tamanho;
-    arvore_t *arvore_binaria;
     arvore_t *arvore_organizada;
 
     arvore_organizada = NULL;
-    arvore_binaria = NULL;
 
     lista = ler_arquivo("Pacientes.csv");
 
-    fila = cria_fila_pacientes(lista);
-
     tamanho = obtem_tamanho(lista);
 
-    paciente_t *buffer[tamanho];
+    paciente_t *buffer[tamanho-1];
+
+    no = obtem_cabeca(lista);
 
     for(x = 0; x <= tamanho-1; x++){
-        paciente_atual = dequeue(fila);
-        buffer[x] = paciente_atual;}
+        paciente_atual = obter_dado(no);
+        buffer[x] = paciente_atual;
+        no = obter_proximo(no);}
 
     x=0;
 
     new_tree(&arvore_organizada, buffer, tamanho-1, x);
 
-    arvore_binaria = heapsort_loop(arvore_organizada, tamanho);
+    x=0;
 
-//    print_preorder(arvore_organizada);
+    organizador(&arvore_organizada, tamanho, x);
 
-//    print_preorder(arvore_organizada);
-
+    print_tree(arvore_organizada, tamanho, x);
 
     libera_dados_list_enc(lista);
-
-    libera_fila(fila);
 
     return 0;
 }
