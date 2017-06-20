2dc7cf6d9ea238f3c3a95def6789813c71adfe12
Adjucaris <importantinho_2@hotmail.com>
2017-06-20 05:28:40 -0300

    Update arvore_bin.h

--- arvore_bin.h
+++ arvore_bin.h
@@ -9,12 +9,16 @@ void print_tree(arvore_t *tree, int tamanho, int indice);
 
 int imprimir(arvore_t *tree, int tamanho, int indice);
 
-arvore_t *heapsort_loop(arvore_t *tree, int tamanho);
-
-arvore_t *heapsort(arvore_t *tree);
-
 void new_tree(arvore_t **tree , paciente_t *paciente_atual[], int maximo, int indice);
 
 int new_tree_loop(arvore_t **tree, paciente_t *paciente_atual[], int maximo, int indice);
+
+void *organizador(arvore_t **tree, int maximo, int indice);
+
+int organizador_loop(arvore_t *tree, int mudar, int indice);
+
+void trocar(arvore_t *maior, arvore_t *menor);
+
+void num_order(arvore_t **tree, int maximo, int indice);
 
 #endif /* ARVORE_BIN_H_ */
