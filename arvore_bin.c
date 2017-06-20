e3b4a9ed9c5b37f1975a28ceea3734a52dd2fdd4
Adjucaris <importantinho_2@hotmail.com>
2017-06-20 05:28:17 -0300

    Update arvore_bin.c

--- arvore_bin.c
+++ arvore_bin.c
@@ -32,12 +32,8 @@ void print_tree(arvore_t *tree, int tamanho, int indice){
 
 int imprimir(arvore_t *tree, int tamanho, int indice){
 
-    //printf("PONTO 3\n");
-
     int ind=indice;
     paciente_t *paciente;
-
-    printf("\n%d\n", ind);
 
     if(ind>tamanho)
         return tamanho+1;
@@ -47,8 +43,6 @@ int imprimir(arvore_t *tree, int tamanho, int indice){
         printf("%s\n", obter_nome(paciente));
         ind++;
         return ind;}
-
-    //printf("PONTO 4\n");
 
     if(tree->left==NULL && tree->right==NULL)
         return ind;
@@ -69,65 +63,11 @@ int imprimir(arvore_t *tree, int tamanho, int indice){
 //    }
 //}
 
-arvore_t *heapsort_loop(arvore_t * tree, int tamanho){
-    int controle;
-    arvore_t *heap1, *heap2;
-
-    for (controle = 0; heap1 != heap2 || controle < tamanho-1; ++controle){
-        if ((controle % 2) == 0)
-            heap1 = heapsort(tree);
-        else
-            heap2 = heapsort(tree);}
-
-    return tree;
-}
-
-arvore_t *heapsort(arvore_t *tree){
-    arvore_t *temporario;
-    paciente_t *dado0, *dado1, *dado2;
-
-    printf("PONTO 1\n");
-    dado0 = tree->dados;
-    printf("PONTO 2\n");
-    dado1 = tree->left->dados;
-    printf("PONTO 3\n");
-    dado2 = tree->right->dados;
-    printf("PONTO 4\n");
-
-    if (tree->left && tree->right)
-   {
-       printf("PONTO 5\n");
-      /* puts lesser value on top of node */
-      if ((obter_prioridade(dado1) < obter_prioridade(dado2)) && (obter_prioridade(dado1) > obter_prioridade(dado0)))
-      {
-          printf("PONTO 6\n");
-         temporario->dados = tree->dados;
-         tree->dados = tree->left->dados;
-         tree->left->dados = temporario->dados;
-      }
-      else if ((obter_prioridade(dado1) > obter_prioridade(dado2)) && (obter_prioridade(dado2) < obter_prioridade(dado0)))
-      {
-          printf("PONTO 7\n");
-            temporario->dados = tree->dados;
-         tree->dados = tree->right->dados;
-         tree->right->dados = temporario->dados;
-      }
-      if (tree->left)
-        printf("PONTO 8\n");
-        heapsort(tree->left);
-      if (tree->right)
-      printf("PONTO 9\n");
-        heapsort(tree->right);
-   }
-   return tree;
-
-}
-
 void new_tree(arvore_t **tree , paciente_t *paciente_atual[], int maximo, int indice){
 
     arvore_t *temp = *tree;
     arvore_t *temp2, *temp3;
-    int t1, t2, u=0;
+    int t1, t2;
     int ind = indice;
     t1 = 2*(ind) + 1;
     t2 = 2*(ind) + 2;
@@ -178,16 +118,12 @@ void new_tree(arvore_t **tree , paciente_t *paciente_atual[], int maximo, int in
 
 int new_tree_loop(arvore_t **tree , paciente_t *paciente_atual[], int maximo, int indice){
 
-    paciente_t *pessoa1, *pessoa2;
     arvore_t *temp = *tree;
     arvore_t *temp2, *temp3;
-    int t1, t2, u;
+    int t1, t2;
     int ind = indice;
     t1 = 2*(ind) + 1;
     t2 = 2*(ind) + 2;
-
-    pessoa1 = paciente_atual[t1];
-    pessoa2 = paciente_atual[t2];
 
     if (ind>maximo)
         return ind;
@@ -201,7 +137,7 @@ int new_tree_loop(arvore_t **tree , paciente_t *paciente_atual[], int maximo, in
         temp2->left =  NULL;
         temp2->right = NULL;
         temp2->numero = t1;
-        temp2->dados = pessoa1;
+        temp2->dados = paciente_atual[t1];
 
         if(t2>maximo)
             return maximo+1;
@@ -210,7 +146,7 @@ int new_tree_loop(arvore_t **tree , paciente_t *paciente_atual[], int maximo, in
         temp3->left =  NULL;
         temp3->right = NULL;
         temp3->numero = t2;
-        temp2->dados = pessoa2;
+        temp3->dados = paciente_atual[t2];
 
         temp->left = temp2;
         temp->right = temp3;
@@ -225,3 +161,113 @@ int new_tree_loop(arvore_t **tree , paciente_t *paciente_atual[], int maximo, in
 
     return ind;
     }
+
+void *organizador(arvore_t **tree, int maximo, int indice){
+
+    printf("PONTO DE TESTE 0\n");
+
+    int mudar=0;
+    int ind=indice;
+    arvore_t *temp = *tree;
+
+    printf("PONTO DE TESTE 1\n");
+
+    while(ind<maximo){
+        printf("fazendo organizaçao do indice %d\n", ind);
+        mudar=organizador_loop(temp, mudar, ind);
+        if(mudar==1){
+            mudar=0;
+            ind++;}
+        printf("voltou no indice %d\n", ind);
+    }
+
+    *tree = temp;
+
+    num_order(&(*tree), maximo, indice);}
+
+int organizador_loop(arvore_t *tree, int mudar, int indice){
+
+    int ind, mud, calamidade;
+    ind = indice;
+    mud = mudar;
+    calamidade = 0;
+
+    if(mud==1){
+        return 1;}
+
+    if(tree->left == NULL){
+        calamidade++;}
+
+    if(tree->right == NULL){
+        calamidade++;}
+
+    if(calamidade==2){
+        if (tree->numero){
+            return 1;}
+        return 0;}
+
+    paciente_t *d_center, *d_left, *d_right;
+    d_center = tree->dados;
+    d_left = tree->left->dados;
+    d_right = tree->right->dados;
+
+
+    if(tree->numero == ind){
+
+        printf("ENTROU NO IF\n");
+
+        if(obter_prioridade(d_left) < obter_prioridade(d_right) && obter_prioridade(d_right) > obter_prioridade(d_center)){
+            printf("Troca para a esquerda\n");
+            trocar(tree->right, tree);}
+
+        else if(obter_prioridade(d_left) > obter_prioridade(d_right) && obter_prioridade(d_left) > obter_prioridade(d_center)){
+            printf("Troca para a direita\n");
+            trocar(tree->left, tree);}
+
+        else if(obter_prioridade(d_left) == obter_prioridade(d_right) && obter_prioridade(d_left) > obter_prioridade(d_center)){
+            printf("Terceira troca (esquerda)\n");
+            trocar(tree->left, tree);}
+
+        else
+            return 1;}
+
+
+
+    printf("Vai para esquerda\n");
+    mud=organizador_loop(tree->left, mud, ind);
+    printf("Vai para direira\n");
+
+    if(mud==1){
+        return 1;}
+
+    mud=organizador_loop(tree->right, mud, ind);
+
+    if(mud==1){
+        return 1;}
+
+    printf("Volta final\n");
+    return 0;
+
+}
+
+void trocar(arvore_t *maior, arvore_t *menor){            //poe tudo do 1 no 2
+    arvore_t *temp;
+    temp = (arvore_t *)malloc(sizeof(arvore_t));
+
+    temp->dados = menor->dados;
+    temp->numero = menor->numero;
+
+    menor->dados = maior->dados;
+    menor->numero = maior->numero;
+
+    maior->dados = temp->dados;
+    maior->numero = temp->numero;
+
+    free(temp);
+}
+
+
+void num_order(arvore_t **tree, int maximo, int indice){
+
+
+}
