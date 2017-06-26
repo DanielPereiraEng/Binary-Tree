fa82392fcbe72ba4dc8d90bf00416ca83c9403fa
Adjucaris <importantinho_2@hotmail.com>
2017-06-25 22:10:14 -0300

    Update arvore_bin.c

--- arvore_bin.c
+++ arvore_bin.c
@@ -38,8 +38,6 @@ void new_tree(arvore_t **tree , paciente_t *pacientes[], int maximo){
 
     ramo_left(&(*tree)->left, pacientes, t1, maximo);
     ramo_right(&(*tree)->right, pacientes, t2, maximo);
-    
-    free(temp);
 
     return;}
 
